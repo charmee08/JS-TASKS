41007490ebc6706581c27df80c64004dd0aef581
charmee08 <24bcs108@kgcas.com>
2025-11-05 16:17:09 +0530

    Delete Task2 directory

--- Task2/app.js
+++
@@ -1,5 +0,0 @@
-function luck() {
-    let name = document.getElementById("username").value;
-    let a = Math.round(Math.random() * 100);
-    document.getElementById("luck").innerText = name + "'s Lucky Number Is: " + a;
-  }
